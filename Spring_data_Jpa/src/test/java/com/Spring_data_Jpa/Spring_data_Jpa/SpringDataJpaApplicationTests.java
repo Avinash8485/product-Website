48533c330fb1527b9f4c8211aa0f40ba8e530289
Avinash8485 <95474773+Avinash8485@users.noreply.github.com>
2025-02-14 18:03:28 +0530

    Add files via upload

---
+++ Spring_data_Jpa/src/test/java/com/Spring_data_Jpa/Spring_data_Jpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spring_data_Jpa.Spring_data_Jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
